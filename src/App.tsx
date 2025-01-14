98b44160276a81f61f9e8564b31b0699e77f24a2
ss1119 <y.sasaki111919@gmail.com>
2025-01-15 00:19:54 +0900

    fix src

--- src/App.tsx
+++ src/App.tsx
@@ -100,7 +100,7 @@ function App() {
                     return (
                       <label key={index}>
                         <img
-                          src={"./public/assets/service" + (index + 1) + ".jpg"}
+                          src={"./assets/service" + (index + 1) + ".jpg"}
                           alt={"service" + (index + 1)}
                           onClick={() => {
                             const newValue = isSelected
